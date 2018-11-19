3f2fa58a8264785e75841cd717384480b0700ecb
rebu1818@gmail.com <Osirishax1337>
2018-11-19 17:28:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/colere/easyfood/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.colere.easyfood;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import info.hoang8f.widget.FButton;
+
+public class MainActivity extends AppCompatActivity {
+
+ /* VIDEO1, TERMINADO
+    VIDEO2, TERMINADO
+    VIDEO3, TERMINADO      */
+
+   FButton btnSignIn, btnSignUp;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = (FButton) findViewById(R.id.btnSignIn);
+        btnSignUp = (FButton)findViewById(R.id.btnSignUp);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() { //llamamos al botón
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() { //llamamos al botón
+            @Override
+            public void onClick(View v) {
+                Intent signUp = new Intent (MainActivity.this,SignUp.class);
+                startActivity(signUp);
+
+            }
+        });
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signIn = new Intent(MainActivity.this,SignIn.class);
+                startActivity(signIn);
+
+            }
+        });
+
+    }
+}
